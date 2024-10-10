bcc6ab7fbb9a0a61f540723c16ed77c1915a144c
user <user@users-MBP.Dlink>
2024-10-10 13:32:53 +0530

    add gfg info

---
+++ work.js
@@ -0,0 +1,20 @@
+const geeksForGeeks ={
+    profileUpdate: {
+        Profile_picture: "",
+        Name: "Rahul Jain",
+        profile_Heading: "",
+        Email_id: "rahul19998jain@gmail.com",
+        Gender: "Male",
+        state: "Utter Pardesh",
+        city: "meerut",
+        ShortBio: "",
+        Contact_Number: "",
+        Technical_skills: ["ReactJS", "JavaScript", "Redux"],
+        Upload_resume: "",
+        Social_profile: ["Linkedin", "Github"],
+        Total_work_experience: "",
+        Notice_period: "",
+        willing_to_relocate: "",
+        Salary_info: "i think not disclose"
+    }
+}
